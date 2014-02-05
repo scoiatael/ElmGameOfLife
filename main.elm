3ac7a55f0ee61be6c50d7524d0ed08332e834a69
scoiatael <czapl.luk@gmail.com>
2014-02-05 15:45:09 +0000

    started fixing

--- main.elm
+++ main.elm
@@ -19,7 +19,7 @@ input = sampleOn (merge (lift (\_ -> () ) clock) Mouse.clicks) ( Input <~ Mouse.
 
 type Button = { onClick : Game -> Game, position : Position, size : Position, text : String }
 data State = Paused | Playing
-type Board = { size : Position, units : Dict Position () }
+type Board = { size : Position, units : Dict.Dict Position () }
 type Game = { buttons : [ Button ], state : State, board : Board }
 
 type Constants = { size : Position, tileSize : Position }
@@ -41,8 +41,9 @@ buttonClick : Position -> Button -> Bool
 buttonClick {x,y} {position, size} = x >= position.x && x < position.x + size.x && y >= position.y && y < position.y + size.y 
 
 buttonHandleClick : [Button] -> Game -> Game
-buttonHandleClick [] = id
-buttonHandleClick ({onClick}::_) = onClick
+buttonHandleClick s = case s of
+  [] -> id
+  ({onClick}::_) -> onClick
 
 handleClickAt : Position -> Game -> Game
 handleClickAt pos ({buttons} as game) = let bs = filter (buttonClick pos) buttons in buttonHandleClick bs game
@@ -69,13 +70,15 @@ circleAt {x,y} = oval constants.tileSize.x constants.tileSize.y |> filled white 
 
 crossAt {x,y} = rect constants.tileSize.x constants.tileSize.y |> outlined defaultLine |> placeTile (x,y)
 
-displayTile d p = if p `lookup` d then circleAt p else crossAt p
+displayTile d p = if p `Dict.lookup` d /= Nothing then circleAt p else crossAt p
 
-displayBoard ({dict} as board) = layer <| map (displayTile dict) <| allPairs board.size.x board.size.y
+allPairs {x,y} = map (\a ->  map (\b -> (a,b)) [0..x-1] ) [0..y-1]
+
+displayBoard ({dict} as board) = layers <| map (displayTile dict) <| allPairs board.size.x board.size.y
 
 display (w,h) {buttons, board, state} = container w h middle <| collage constants.size.x constants.size.y [
   displayState state,
   displayBoard board
-  ] ++ map displayButton buttons
+  ] ++ map displayTile buttons
 
-main = display <~ Windows.dimensions ~ gameState
+main = display <~ Window.dimensions ~ gameState
